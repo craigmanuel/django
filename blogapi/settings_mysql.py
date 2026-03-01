a78d30fa242480b72b9bee494302fddcd41d0e0b
Craig <141469554+craigmanuel@users.noreply.github.com>
2026-03-01 18:50:10 +0800

    Create settings_mysql.py for MySQL database configuration

---
+++ blogapi/settings_mysql.py
@@ -0,0 +1,11 @@
+# MySQL Database Configuration for Part-2
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'apidb',
+        'USER': 'root',
+        'PASSWORD': 'My81ack80r&',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
